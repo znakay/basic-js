d71b1b03b04264f433b147824d88a2305aabb6f4
Andrey Znak <a.y.znak@gmail.com>
2020-09-21 21:28:31 +0300

    update: complite task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  
+  let names = members.map(item => typeof(item) == 'string' ? item.trim() : false);
+
+  let dreamTeam = [];
+  
+  names.forEach(item => {
+    if (item) {
+      dreamTeam.push(item[0].toUpperCase());
+    }
+  })
+
+  return dreamTeam.sort().join(''); 
 };
+
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined)
+    return 'Unable to determine the time of year!';
+
+  if (Date.parse(+date)) throw new Error;
+
+  let month = date.getMonth();
+
+  switch (month) {
+    case 0:
+    case 1:
+    case 11: return "winter";
+    case 2:
+    case 3:
+    case 4: return "spring";
+    case 5:
+    case 6:
+    case 7: return "summer";
+    case 8:
+    case 9:
+    case 10: return "autumn";
+    default: throw new Error('invalid value');
+  }
 };
