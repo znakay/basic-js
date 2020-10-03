1fe796f48e228a24cc00467762b792c7b56fcc3d
Andrey Znak <a.y.znak@gmail.com>
2020-10-03 23:28:29 +0300

    feat: add disicion

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -2,7 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   const turns = (2 ** disksNumber) - 1;
-  const seconds = Math.trunc((3600 / turnsSpeed) * turnseturn {
+  const seconds = Math.trunc((3600 / turnsSpeed) * turns);
+  
+  return {
     turns,
     seconds,
   }
