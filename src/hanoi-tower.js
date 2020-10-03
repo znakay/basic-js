df0ec253828be4bcdfc715bb15583934392958f9
Andrey Znak <a.y.znak@gmail.com>
2020-10-03 23:24:12 +0300

    feat: add disicion

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  const turns = (2 ** disksNumber) - 1;
+  const seconds = Math.trunc((3600 / turnsSpeed) * turnseturn {
+    turns,
+    seconds,
+  }
+}
