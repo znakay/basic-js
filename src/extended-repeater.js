eb8c0369383b9879aa5cda95922b3e50c5df4487
Andrey Znak <a.y.znak@gmail.com>
2020-10-15 00:21:39 +0300

    feat: add disicion

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  str = isString(str);
+  let _separator = defineSeparator(options.separator, '+');
+  let _additionSeparator = defineSeparator(options.additionSeparator, '|');
+  let newStr = new Array(options.repeatTimes);
+  let _addition = '';
+
+  if ('addition' in options) {
+    _addition = addAddtition(options.addition, options.additionRepeatTimes, _additionSeparator);
+  }
+  let stringWithAddition = str + _addition;
   
+  newStr.fill(stringWithAddition, 0);
+
+  return newStr.join(_separator);
+};
+
+function addAddtition(addition, additionRepeatTimes, additionSeparator) {
+  addition = isString(addition);
+  return new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+}
+
+function defineSeparator(separator, def) {
+  if (separator != null) {
+    return separator;
+  } else {
+    return def;
+  }
+}
+
+function isString(value) {
+  if (value == null) return 'null';
+  return value.toString();
+}
